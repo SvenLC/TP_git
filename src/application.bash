a07deb097fb52c7d8f0d64755b89dce936e1865a
Sven Le Cann <sven.lecann@live.fr>
2018-03-12 15:46:47 +0100

    add var to script

--- src/application.bash
+++ src/application.bash
@@ -1,4 +1,8 @@
 ### Application.bash - 2018 - Sven Le Cann ###
-echo "version 1.1"
+version = "2"
+build_hour="11h00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
 echo "Copyright Company - 201x"
 echo "Build : 10:00"
